0326a1a23cba9a7df4bda9c10a939f76e28dba24
aarikakaur92 <aarikakaur92@gmail.com>
2026-03-22 15:12:32 -0600

    add TTL cache for tool handlers

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@
 
 export { MCPServer } from './server';
 export type { MCPServerOptions, ToolHandler, ResourceHandler, PromptHandler } from './types';
+export { withCache, createToolCache } from './middleware/cache';
+export type { CacheOptions } from './middleware/cache';
