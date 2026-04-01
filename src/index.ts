e501a232894d7b89c788692ce4b530adc747f1e3
Moustache4767 <Moustache4767@proton.me>
2026-03-31 20:54:39 -0600

    add HTTP/SSE transport with CORS support

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@
 
 export { MCPServer } from './server';
 export type { MCPServerOptions, ToolHandler, ResourceHandler, PromptHandler } from './types';
+export { StdioServer } from './transport/stdio';
+export { HttpMCPServer } from './transport/http';
+export type { HttpServerOptions } from './transport/http';
 export { withCache, createToolCache } from './middleware/cache';
 export type { CacheOptions } from './middleware/cache';
 export { MCPValidationError, formatZodError, validationErrorResponse, RPC_ERRORS, MCP_ERRORS } from './errors';
